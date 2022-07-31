55c4b97b2c7572648390bfbfb3902eeff36edbd6
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2022-07-31 20:18:00 +0700

    pull request

--- js/script.js
+++ js/script.js
@@ -23,12 +23,21 @@ function showSlides() {
 
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000);
+  setTimeout(showSlides, 5000);
 }
 
 
 
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("blue-mode");
 
+}
+
+function darktema() {
+  var element = document.body;
+  element.classList.toggle("dark-mode-github")
+}
 
 
 $(document).ready(function(){
