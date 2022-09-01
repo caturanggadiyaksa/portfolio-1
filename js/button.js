3d71d9e44c01e4fbcd932e3699820255e9fa29c5
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2022-09-01 09:49:20 +0700

    initial commit

---
+++ js/button.js
@@ -0,0 +1,8 @@
+function humbergerMenu() {
+    var x = document.getElementById("humberger-menu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
